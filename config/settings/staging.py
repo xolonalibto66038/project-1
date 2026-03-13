3623b4ce2c42a7b1a288a2a90efab1b12a4bcf0c
xolonalibto66038 <xolonalibto66038@gmail.com>
2026-03-13 18:18:17 +0100

    try to fix staticfiles problem by replacing the storage backend : "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -31,7 +31,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
