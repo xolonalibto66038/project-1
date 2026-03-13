59cdc66696afdeca18041ff47473584d1bb335db
xolonalibto66038 <xolonalibto66038@gmail.com>
2026-03-13 18:41:33 +0100

    try finders to fix static files problem

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -37,6 +37,11 @@ STORAGES = {
     },
 }
 
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": env("CLOUDINARY_CLOUD_NAME"),
     "API_KEY": env("CLOUDINARY_API_KEY"),
